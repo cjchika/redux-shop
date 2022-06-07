f7f87fbb18f6e2490342095723696132447eeb5d
Chijioke Chika <8pluscreative@gmail.com>
2022-06-07 16:37:32 -0400

    Update

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -25,6 +25,19 @@ const cartSlice = createSlice({
 			}
     },
 
-    remoreItemFromCart() {},
+    remoreItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      if(existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id)
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice = existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
