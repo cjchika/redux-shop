c5ff7ca778e43aa9a7a3171d57ab6712f0a0d584
Chijioke Chika <8pluscreative@gmail.com>
2022-06-08 00:53:37 -0400

    Redux and Firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisible)
-  const cart = useSelector((state) => state.cart)
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch('https://food-project-e16bf-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
-    })
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "Pending",
+          title: "Sending",
+          message: "Sending cart data",
+        })
+      );
+      const response = await fetch(
+        "https://food-project-e16bf-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+    };
+
+    if (!response.ok) {
+      throw new Error("Sending cart data failed.");
+    }
+
+    dispatch(
+      uiActions.showNotification({
+        status: "success",
+        title: "Success",
+        message: "Cart data sent successfully",
+      })
+    );
+
+    sendCartData();
   }, [cart]);
 
   return (
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,14 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
-  name: 'ui', 
-	initialState: { cartIsVisible: false },
-	reducers: {
-		toggle(state) {
-			state.cartIsVisible = !state.cartIsVisible;
-		}
-	}
-
+  name: "ui",
+  initialState: { cartIsVisible: false, notifificiation: null },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notifificiation = {
+        status: action.payload.status,
+        titl: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
 });
 
 export const uiActions = uiSlice.actions;
